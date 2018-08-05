da0e6814a50b1c9cb363b95ee1a946c2fe4ac14e
James Lafferty <james@j-lafferty.net>
2018-08-04 21:47:55 -0700

    restrict flexbox to total pixel size and create etch-a-sketch board

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
+let gameBoard = document.querySelector('#gameboard');
+var mouse = document.getElementById("gameboard");
+
+var gridLines = "";
+var gridSquares = "";
 
 function genDivs (v) {
-    let e = document.querySelector('#gameboard');
 
-    //create rows within the only div written in html page
+    //create rows within the 'gameboard' div in html page
     for (let i = 0; i < v; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
@@ -10,18 +14,19 @@ function genDivs (v) {
         //create cells within each row
         for (let x = 1; x <= v; x++) {
             let cell = document.createElement('div');
-            cell.classList.add('gridsquare');
+            cell.classList.add('gridSquare');
             //cell.id = "square";
             row.appendChild(cell);
         };
 
-        e.appendChild(row);
+        gameBoard.appendChild(row);
     };
+    gridSquares = document.querySelectorAll(".gridSquare");
+    gridLines = document.querySelectorAll(".row");
     
     //document.getElementById("container").innerText = e.innerHTML;
 };
 
-var mouse = document.getElementById("gameboard");
 
 mouse.addEventListener("mouseover", function( e ) {   
     // highlight the mouseover target
